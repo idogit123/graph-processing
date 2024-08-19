787c817101ad5b0b5cd54be5c0abcb75467292b5
Ido V. Z <programerido@gmail.com>
2024-08-19 14:56:13 +0300

    4 Graphs

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 import { Graph } from './Graph.js'
 
-const graph1 = new Graph('p1', 300, 0, 500, 200, 0.14, 0.08, 20, 130, 255)
-const graph2 = new Graph('p2', 300, 0, 500, 400, 0.14, 0.08, 100, 255, 130)
-// const graph3 = new Graph('p3', 300, 0, 100, 70, 10, 0.08, 255, 130, 20)
+const graph1 = new Graph('p1', 300, 0, 60_000, 200   , 0.02, 0.08, 20 , 130, 255)
+const graph2 = new Graph('p2', 300, 0, 60_000, 400   , 0.02, 0.08, 100, 255, 130)
+const graph3 = new Graph('p3', 300, 0, 60_000, 48_000, 0.10, 0.08, 20 , 130, 255)
+const graph4 = new Graph('p4', 300, 0, 60_000, 24_000, 0.10, 0.08, 100, 255, 130)

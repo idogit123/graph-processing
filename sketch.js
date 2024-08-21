211bdd38200cf81f5ffd0dc2e7d373fb42b75a4c
Ido V. Z <programerido@gmail.com>
2024-08-21 16:30:22 +0300

    Add tryStoreSync

--- sketch.js
+++ sketch.js
@@ -2,5 +2,5 @@ import { Graph } from './Graph.js'
 
 const graph1 = new Graph('p1', 300, 0, 60_000, 200   , 0.02, 0.08, 20 , 130, 255)
 const graph2 = new Graph('p2', 300, 0, 60_000, 400   , 0.02, 0.08, 100, 255, 130)
-const graph3 = new Graph('p3', 300, 0, 60_000, 48_000, 0.10, 0.08, 20 , 130, 255)
+const graph3 = new Graph('p3', 300, 0, 60_000, 51_000, 0.10, 0.08, 20 , 130, 255)
 const graph4 = new Graph('p4', 300, 0, 60_000, 24_000, 0.10, 0.08, 100, 255, 130)
